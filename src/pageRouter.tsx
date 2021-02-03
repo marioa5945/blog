5277125ae558fdab7c04ee54128493d44092698f
marioa <marioa5945@gmail.com>
2021-02-03 10:10:45 +0800

    Code-Splitting

--- src/pageRouter.tsx
+++ src/pageRouter.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { Suspense } from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import { createHashHistory } from 'history';
 import { Provider } from 'react-redux';
@@ -6,21 +6,23 @@ import { store } from './reducers';
 
 const history = createHashHistory();
 
-import PageHome from './container/home/';
-import Blog from './container/blog/';
-import Page404 from './container/status/';
+const PageHome = React.lazy(() => import('./container/home/'));
+const Blog = React.lazy(() => import('./container/blog/'));
+const Page404 = React.lazy(() => import('./container/status/'));
 
 const PageRouter: React.FC = () => {
   return (
-    <Provider store={store}>
-      <Router history={history}>
-        <Switch>
-          <Route exact={true} path={'/'} component={PageHome} />
-          <Route path={'/blog'} component={Blog} />
-          <Route component={Page404} />
-        </Switch>
-      </Router>
-    </Provider>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Provider store={store}>
+        <Router history={history}>
+          <Switch>
+            <Route exact={true} path={'/'} component={PageHome} />
+            <Route path={'/blog'} component={Blog} />
+            <Route component={Page404} />
+          </Switch>
+        </Router>
+      </Provider>
+    </Suspense>
   );
 };
 
